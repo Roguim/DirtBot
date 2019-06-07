721f2807fa55a701acbba9b19f24c340b87b910b
d4julian <julianubico@icloud.com>
2019-06-07 15:31:26 -0400

    add & remove message reaction event

--- src/main/java/net/dirtcraft/dirtbot/modules/ServerRolesModule.java
+++ src/main/java/net/dirtcraft/dirtbot/modules/ServerRolesModule.java
@@ -8,11 +8,10 @@ import net.dirtcraft.dirtbot.internal.embeds.EmbedUtils;
 import net.dirtcraft.dirtbot.internal.modules.Module;
 import net.dirtcraft.dirtbot.internal.modules.ModuleClass;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionRemoveEvent;
 
 import java.time.Instant;
-
-import static net.dirtcraft.dirtbot.DirtBot.getConfig;
 
 @ModuleClass(classLiteral = ServerRolesModule.class)
 public class ServerRolesModule extends Module<ServerRolesModule.ConfigDataServerRoles, ServerRolesModule.EmbedUtilsServerRoles> {
@@ -57,7 +56,94 @@ public class ServerRolesModule extends Module<ServerRolesModule.ConfigDataServer
     // DO ALL OF YOUR EVENT LISTENERS DOWN HERE
     // THIS IS AN EXAMPLE LISTENER IN THIS FORMAT, YOU CAN DELETE THIS ONE IF YOU DON'T NEED IT
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if (event.getMessageId().equals("538574783319638026")) {
+            switch (event.getReactionEmote().getName().toLowerCase()) {
+                case "stoneblock":
+                    giveRole(event);
+                    break;
+                case "projectozone":
+                    giveRole(event);
+                    break;
+                case "continuum":
+                    giveRole(event);
+                    break;
+                case "infinityevolved":
+                    giveRole(event);
+                    break;
+                case "skyfactory":
+                    giveRole(event);
+                    break;
+                case "revelation":
+                    giveRole(event);
+                    break;
+                case "interactions":
+                    giveRole(event);
+                    break;
+                case "skyadventures":
+                    giveRole(event);
+                    break;
+                case "ultimatereloaded":
+                    giveRole(event);
+                    break;
+                case "skyodyssey":
+                    giveRole(event);
+                    break;
+                case "direwolf20":
+                    giveRole(event);
+                    break;
+            }
+        }
+    }
 
+    @Override
+    public void onMessageReactionRemove(MessageReactionRemoveEvent event) {
+        if (event.getMessageId().equals("538574783319638026")) {
+            switch (event.getReactionEmote().getName().toLowerCase()) {
+                case "stoneblock":
+                    removeRole(event);
+                    break;
+                case "projectozone":
+                    removeRole(event);
+                    break;
+                case "continuum":
+                    removeRole(event);
+                    break;
+                case "infinityevolved":
+                    removeRole(event);
+                    break;
+                case "skyfactory":
+                    removeRole(event);
+                    break;
+                case "revelation":
+                    removeRole(event);
+                    break;
+                case "interactions":
+                    removeRole(event);
+                    break;
+                case "skyadventures":
+                    removeRole(event);
+                    break;
+                case "ultimatereloaded":
+                    removeRole(event);
+                    break;
+                case "skyodyssey":
+                    removeRole(event);
+                    break;
+                case "direwolf20":
+                    removeRole(event);
+                    break;
+            }
+        }
+    }
+
+    private void giveRole(MessageReactionAddEvent event) {
+        event.getGuild().getRolesByName(event.getReactionEmote().getName().toLowerCase(), true).forEach(role ->
+                event.getGuild().getController().addRolesToMember(event.getMember(), role).queue());
+    }
+
+    private void removeRole(MessageReactionRemoveEvent event) {
+        event.getGuild().getRolesByName(event.getReactionEmote().getName().toLowerCase(), true).forEach(role ->
+                event.getGuild().getController().removeRolesFromMember(event.getMember(), role).queue());
     }
 }
