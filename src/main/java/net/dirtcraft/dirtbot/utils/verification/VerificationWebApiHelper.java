3806820ff38ffe1d7f0a1a8f89bd44da9a00bfbd
= <ShinyTourettesX@gmail.com>
2019-07-05 07:45:15 +0800

    quick method name change

--- src/main/java/net/dirtcraft/dirtbot/utils/verification/VerificationWebApiHelper.java
+++ src/main/java/net/dirtcraft/dirtbot/utils/verification/VerificationWebApiHelper.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VerificationWebApiHelper {
-    public static String getLastUsername(String uuid) {
+    public static String getCurrentUsername(String uuid) {
         uuid = uuid.replace("-","");
         String currentName = null;
         HttpURLConnection con = null;
